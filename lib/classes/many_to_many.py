e42045e61e525e6de9ae1233acc98f2972a3e496
njoroofficial <mosesn579@gmail.com>
2026-01-11 11:38:58 +0300

    set initializers and properties

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,36 @@
 class Article:
+    all_articles = [] # track every article created
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if hasattr(self, "_title"):
+            return # cannot change after instantiation
+        if isinstance(title, str) and 5 <= len(title) <= 50:
+            self._title = title
         
 class Author:
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, author_name):
+        if hasattr(self, "_name"):
+            return
+        if isinstance(author_name, str) and len(author_name) > 0:
+            self._name = author_name
 
     def articles(self):
         pass
@@ -24,6 +48,24 @@ class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 2 <= len(value) <= 16:
+            self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
 
     def articles(self):
         pass
