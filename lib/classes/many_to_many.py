f4d8614c09f29f383fe509c1b7da82b2a567cddd
njoroofficial <mosesn579@gmail.com>
2026-01-11 11:52:19 +0300

    added object relationship methods and properties

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -5,6 +5,7 @@ class Article:
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all_articles.append(self)
 
     @property
     def title(self):
@@ -16,6 +17,24 @@ class Article:
             return # cannot change after instantiation
         if isinstance(title, str) and 5 <= len(title) <= 50:
             self._title = title
+
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, value):
+        if isinstance(value, Author):
+            self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self, value):
+        if isinstance(value, Magazine):
+            self._magazine = value
         
 class Author:
     def __init__(self, name):
@@ -33,10 +52,10 @@ class Author:
             self._name = author_name
 
     def articles(self):
-        pass
+        return [article for article in Article.all_articles if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set([article.magazine for article in self.articles()]))
 
     def add_article(self, magazine, title):
         pass
@@ -68,10 +87,10 @@ class Magazine:
             self._category = value
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set([article.author for article in self.articles()]))
 
     def article_titles(self):
         pass
